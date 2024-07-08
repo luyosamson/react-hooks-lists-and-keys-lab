60b78c4402317158733aaba3ed3d48841b2db1e5
facelessDev <luyosamson@gmail.com>
2024-07-08 19:39:36 +0300

    changed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import users from "../data/user"
 
 function ProjectItem({ name, about, technologies }) {
   return (
@@ -6,7 +7,9 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {/* render a <span> for each technology in the technologies array */
+        technologies.map((tech,index)=>(<span key={index}>{tech}</span>))
+        }
       </div>
     </div>
   );
